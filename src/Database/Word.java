45eb419f47c71a34fa9fa549fdb19bd7e8a56f96
Hieu H Vu <hieuvu927@gmail.com>
2021-05-28 13:39:56 +0700

    Minor modifications

--- src/Database/Word.java
+++ src/Database/Word.java
@@ -2,6 +2,7 @@ package Database;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+
 import java.util.List;
 import java.util.Objects;
 
@@ -10,34 +11,55 @@ public class Word {
     private StringProperty wordMeaning;
     private String html;
 
-    public Word(){
+    public Word() {
         wordTarget = wordTargetProperty();
         wordMeaning = wordMeaningProperty();
         html = "";
     }
-    public Word(String target, String explain, String html){
+
+    public Word(String target, String explain, String html) {
         wordTargetProperty().set(target);
         wordMeaningProperty().set(explain);
-        this.html = html;}
-    public Word(String target){wordTargetProperty().set(target);}
+        this.html = html;
+    }
 
-    public String getWord(){return wordTargetProperty().get();}
-    public String getMeaning(){return wordMeaningProperty().get();}
-    public String getHtml(){return html;}
-    public StringProperty wordTargetProperty(){
+    public Word(String target) {
+        wordTargetProperty().set(target);
+    }
+
+    public String getWord() {
+        return wordTargetProperty().get();
+    }
+
+    public String getMeaning() {
+        return wordMeaningProperty().get();
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public StringProperty wordTargetProperty() {
         if (wordTarget == null) wordTarget = new SimpleStringProperty(this, "wordTarget");
         return wordTarget;
     }
-    public void setWord(String target){wordTargetProperty().set(target);}
-    public void setMeaning(String explain){
-        wordMeaningProperty().set(explain);}
-    public StringProperty wordMeaningProperty(){
+
+    public void setWord(String target) {
+        wordTargetProperty().set(target);
+    }
+
+    public void setMeaning(String explain) {
+        wordMeaningProperty().set(explain);
+    }
+
+    public StringProperty wordMeaningProperty() {
         if (wordMeaning == null) wordMeaning = new SimpleStringProperty(this, "wordMeaning");
         return wordMeaning;
     }
 
     /**
      * compare two words
+     *
      * @param o word to compare against
      * @return true if word objects' word and definition are the same
      */
@@ -46,7 +68,7 @@ public class Word {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Word word = (Word) o;
-        if (word.getMeaning() == null ) return false;
+        if (word.getMeaning() == null) return false;
 
         //intuitive definition of equal
         boolean sameWord = this.getWord().equals(word.getWord());
@@ -61,11 +83,12 @@ public class Word {
 
     /**
      * checks if this word is already in a word list
+     *
      * @param wordList list to check against
      * @return true if duplicated
      */
-    public boolean duplicated(List<Word> wordList){
-        for (Word word : wordList){
+    public boolean duplicated(List<Word> wordList) {
+        for (Word word : wordList) {
             if (this.equals(word)) return true;
         }
         return false;
